6be265f44c83c137e8830a1fdd70e13bd4708928
Adebayo Adeniyan <adebeslick@gmail.com>
2016-12-16 17:33:32 +0100

    first commit

--- src/main/java/com/adebayo/javafxaddapp/main/MainApp.java
+++ src/main/java/com/adebayo/javafxaddapp/main/MainApp.java
@@ -1,37 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.adebayo.javafxaddapp.main;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
+/**
+ *
+ * @author botosoft
+ */
 public class MainApp extends Application {
-
+    
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+       this.primaryStage = primaryStage;
+       this.primaryStage.setTitle("");
     }
 
     /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-
+    
+    
+/**
+ * returns the main page
+ * @return 
+ */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+    
+    
+    
+    
 }
